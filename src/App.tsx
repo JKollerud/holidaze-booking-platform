105c4e7fe4b00efe0dee56b0f98c481e12058345
JKollerud <j_kollerud@hotmail.com>
2026-04-15 10:04:17 +0200

    setup react, typescript, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+      Holidaze is live!
+    </div>
+  );
+}
+
+export default App;
